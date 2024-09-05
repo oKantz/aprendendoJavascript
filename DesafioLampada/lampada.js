d9186c410ebe72dd225ef3a969aecc62123e2076
kantz <ydarkzinhue@gmail.com>
2024-09-05 14:28:46 -0300

    DesafioLampadaCompleto

--- DesafioLampada/lampada.js
+++ DesafioLampada/lampada.js
@@ -1,34 +1,34 @@
-
-
-const lampada = document.getElementById('lampada'); 
+const lampada = document.getElementById('lampada');
 // Const: Uma varíavel cujo o valor não pode ser alterado após já ter sido atribuido.
 //Document: É um objeto global em JavaScript que representa o documento HTML carregado no navegador
 //getElementById('lampada'): É um método do objeto document que retorna o elemento HTML com o id especificado. 
 //Neste caso, getElementById está buscando um elemento com o id de "lampada".
 //'lampada': É o valor do id que você atribuiu ao elemento <img> no HTML. Isso deve corresponder ao valor usado no atributo id do elemento HTML que você deseja selecionar.
 
-function lampada{
+function controlarLampada(event) {
+    if (lampada.src.includes('multimidia/lampada-quebrada.jpg')) {
+        // Se a lâmpada estiver quebrada, não faz nada.
+        return;
+    }
 
-
-
-lampada.addEventListener('mouseover', () => {
-    if (lampada.src.includes('lampada_apagada.png')) {
-    lampada.src = 'lampada_acesa.jpg';
+    switch (event.type) {
+        case 'mouseover':
+            if (lampada.src.includes('multimidia/lampada-apagada.jpg')) {
+                lampada.src = 'multimidia/lampada-acesa.jpg';
+            }
+            break;
+        case 'mouseout':
+            if (lampada.src.includes('multimidia/lampada-acesa.jpg')) {
+                lampada.src = 'multimidia/lampada-apagada.jpg';
+            }
+            break;
+        case 'click':
+            lampada.src = 'multimidia/lampada-quebrada.jpg';
+            break;
+    }
 }
-});
 
-lampada.addEventListener('mouseout', () => {
-if (lampada.src.includes('lampada_acendida.png')) {
-    lampada.src = 'lampada_apagada.jpg';
-}
-});
-
-lampada.addEventListener('click', () => {
-lampada.src = 'lampada_quebrada.jpg';
-});
-
-// A lâmpada resetará sua condição caso recarregue a página
-window.addEventListener('load', () => {
-lampada.src = 'lampada_apagada.jpg';
-});
-}
+// Adiciona os ouvintes de eventos à lâmpada
+lampada.addEventListener('mouseover', controlarLampada);
+lampada.addEventListener('mouseout', controlarLampada);
+lampada.addEventListener('click', controlarLampada);
