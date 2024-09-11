04b7ae47fa515d86ee7c1b7d0ff99d6dfbad4824
pereira <ydarkzinhue@gmail.com>
2024-09-11 17:21:43 -0300

    AxXaxAX

--- Desafios/Desafio3/Desafio3.js
+++ Desafios/Desafio3/Desafio3.js
@@ -8,10 +8,10 @@ function numero(){
     
         // Caso seja um número, o número digitado será adicionado as respectivas variaveis abaixo
         let antecessor = num1 - 1; // Aqui será adicionado o num1, menos 1 que dará o antecessor
-        let sucessor = num1 + 1; // // Aqui será adicionado o num1, mais 1 que dará o sucessor
+        let sucessor = num1 + 1; // Aqui será adicionado o num1, mais 1 que dará o sucessor
     
     
         alert('O Antecessor é: ' + antecessor + '\nO Sucessor é: ' + sucessor);
        
-    
+        
     }
---
+++ Desafios/Desafio7/desafio7.js
@@ -0,0 +1,7 @@
+function conversao(){
+    const dolar = Number(prompt("por favor, insira a cotação do dolar: "));
+
+    
+    const num1 = Number(prompt('Insira quanto você tem na carteira: '));
+
+}
